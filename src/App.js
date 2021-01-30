79ab94a258903d6930c25c6754ae0b4f898302a6
marcin feleniak <feleniak@gmail.com>
2021-01-30 20:33:03 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
 function App() {
+  const [date, setDate] = useState(tomorrow.toISOString().slice(0, 10));
+  const [garbage, setGarbage] = useState('ładuję...');
+
+  useEffect(() => {
+    const url = 'https://hcgxeeks66.execute-api.us-east-1.amazonaws.com/default/garbage-collector?';
+    const params = new URLSearchParams();
+    params.append('date', date.split('-').reverse().join('.'));
+    setGarbage('sprawdzam...');
+
+    (async () => {
+      try {
+        const response = await fetch(url + params);
+        const json = await response.json();
+        setGarbage(json.garbage);
+      } catch(e) {
+        setGarbage('błędy errory');
+      }
+    })();
+
+  }, [date]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Co wywożą</h4>
+        <input type="date" defaultValue={date} onChange={ev => setDate(ev.target.value)}/>
+        <h3>{ garbage }</h3>
       </header>
     </div>
   );
